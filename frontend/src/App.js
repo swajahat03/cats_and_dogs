6c491e1e56c3a972dacd9381f6cb91801d157c6f
Arun Shriram <ashriram@calpoly.edu>
2020-02-10 16:42:30 -0800

    Added clickable images that route to home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,17 +20,23 @@ function App() {
         <Row gutter={16}>
           <Col span={8}>
             <Card title="kitten1" style={{ margin: 30, height: 300, width: 300}}>
-              <img src={kitten1} alt="Kitten1" style={{width: 250}}></img>
+              <a href="/">
+                <img src={kitten1} alt="Kitten1" style={{width: 250}}></img>
+              </a>
             </Card>
           </Col>
           <Col span={8}>
             <Card title="puppy1" style={{ margin: 30, height: 300, width: 300}}>
-              <img src={puppy1} alt="Puppy1" style={{width: 250}}></img>
+              <a href="/">
+                <img src={puppy1} alt="Puppy1" style={{width: 250}}></img>
+              </a>
             </Card>
           </Col>
           <Col span={8}>
             <Card title="kitten2" style={{ margin: 30, height: 300, width: 300}}>
-              <img src={kitten2} alt="Kitten2" style={{width: 250}}></img>
+              <a href="/">
+                <img src={kitten2} alt="Kitten2" style={{width: 250}}></img>
+              </a>
             </Card>
           </Col>
           
@@ -39,17 +45,23 @@ function App() {
         <Row gutter={16}>
           <Col span={8}>
             <Card title="puppy2" style={{ margin: 30, height: 300, width: 300}}>
-              <img src={puppy2} alt="Puppy2" style={{width: 250}}></img>  
+              <a href="/">
+                <img src={puppy2} alt="Puppy2" style={{width: 250}}></img>  
+              </a>
             </Card>
           </Col>
           <Col span={8}>
             <Card title="kitten3" style={{ margin: 30, height: 300, width: 300}}>
-              <img src={kitten3} alt="Kitten3" style={{width: 250}}></img>
+              <a href="/">
+                <img src={kitten3} alt="Kitten3" style={{width: 250}}></img>
+              </a>
             </Card>
           </Col>
           <Col span={8}>
             <Card title="puppy3" style={{ margin: 30, height: 300, width: 300}}>
-              <img src={puppy3} alt="Puppy3" style={{width: 250}}></img>
+              <a href="/">
+                <img src={puppy3} alt="Puppy3" style={{width: 250}}></img>
+              </a>
             </Card>
           </Col>
           
